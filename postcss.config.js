2dce015f601a37d6b609ddb59f568e7e3c110e23
Jason Nall <jsonnull@gmail.com>
2017-01-19 05:00:34 -0500

    Update postcss plugin configuration.

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  parser: 'postcss-scss',
   plugins: [
-    require('postcss-partial-import')({
+    require('postcss-import')({
       path: 'src/styles'
     }),
+    require('postcss-nested'),
     require('postcss-advanced-variables')(),
-    require('postcss-extend')()
+    require('postcss-extend')(),
+    require('autoprefixer')
   ]
 }
