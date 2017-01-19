3ff4fc1a1982cee2fba08d89e7c1b2d921af1d96
Jason Nall <jsonnull@gmail.com>
2017-01-19 05:04:27 -0500

    Prevent error saving preferences for user which is not logged in.

--- src/backend/middleware/firebase/savePreferences.js
+++ src/backend/middleware/firebase/savePreferences.js
@@ -1,4 +1,9 @@
 export default function savePreferences (firebase, store, action) {
+  // Don't attempt to save preferences if there's no currentUser
+  if (firebase.auth.currentUser == null) {
+    return
+  }
+
   const uid = firebase.auth.currentUser.uid
   const state = store.getState()
   firebase.database.ref(`prefs/${uid}`).set(state.preferences)
