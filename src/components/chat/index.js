6caa90f43eb27b598b1aa7cc2b9146d81e43b4a2
Jason Nall <jsonnull@gmail.com>
2017-01-19 06:55:50 -0500

    Make component directories lower-case.

--- src/components/chat/index.js
+++ src/components/chat/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import Compose from './Compose'
+import Compose from './compose'
 import {
   sendMessage,
   toggleChatPin
--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import Chat from './Chat'
-import Login from './Login'
-import Map from './Map'
-import Profile from './User'
-import Sidebar from './Sidebar'
+import Chat from './chat'
+import Login from './login'
+import Map from './map'
+import Profile from './user'
+import Sidebar from './sidebar'
 
 class Frontend extends React.Component {
   render () {
