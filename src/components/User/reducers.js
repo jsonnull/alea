21f6a1f828a692dfd2e383082d83714b31b8c971
Jason Nall <jsonnull@gmail.com>
2017-01-19 05:33:49 -0500

    Clean up reducer console logs.

--- src/components/User/reducers.js
+++ src/components/User/reducers.js
@@ -11,13 +11,11 @@ const initialState = {
 export default function reducer (state = initialState, action) {
   switch (action.type) {
     case UPDATE_USER:
-      console.log(action)
       let ret = Object.assign({}, state, {
         isLoggedIn: state.isLoggedIn || action.user.isLoggedIn,
         displayName: action.user.displayName || 'anonymous',
         photoURL: action.user.photoURL || '/img/default.png'
       })
-      console.log(ret)
       return ret
     default:
       return state
