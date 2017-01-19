fd3fa72f198b37da46dc3d95e63b6628a487a25a
Jason Nall <jsonnull@gmail.com>
2017-01-19 05:30:52 -0500

    Add sidebar tabs.

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,17 +2,29 @@ import React from 'react'
 import { connect } from 'react-redux'
 import styles from './style.css'
 
+const MenuItem = (props) => {
+  return (
+    <div className={ styles.menuItem }>
+      <i className={`fa ${props.icon}`}></i>
+    </div>
+  )
+}
+
 class Sidebar extends React.Component {
   render () {
+    const buttons = [
+      ['Home', 'fa-globe'],
+      ['Profile', 'fa-user']
+    ]
     return (
-      <div className={styles.sidebar}>
-        <div className={styles.header}>
-          <h1 className={styles.headerTitle}>
-            Avaland
-          </h1>
-          <h2 className={styles.headerSubtitle}>
-            Act I
-          </h2>
+      <div className={ styles.sidebar }>
+        <div className={ styles.menu }>
+          {buttons.map(button =>
+            {
+              const [name, icon] = button
+              return <MenuItem name={name} icon={icon} />
+            }
+          )}
         </div>
       </div>
     )
