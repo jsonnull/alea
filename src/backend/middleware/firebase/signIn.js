5f70d122871e33aacb41bd3685eb09b57f47795b
Jason Nall <jsonnull@gmail.com>
2017-01-20 02:29:39 -0500

    Automatically load messages on login.

--- src/backend/middleware/firebase/signIn.js
+++ src/backend/middleware/firebase/signIn.js
@@ -1,12 +1,11 @@
-import { hideModal } from '../../../actions'
+// import { hideModal } from '../../../actions'
 
 export default function signIn (firebase, store, action) {
   const { email, password } = action
   firebase.auth
     .signInWithEmailAndPassword(email, password)
     .then(() => {
-      store.dispatch(hideModal())
-      store.dispatch({ type: 'LOAD_MESSAGES' })
+      firebase.loadMessages()
     })
     .catch(e => console.error(e))
 }
