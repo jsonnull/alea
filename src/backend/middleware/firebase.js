70ce6593f68e0d6751d6e356a4f8893e123b3c1b
Jason Nall <jsonnull@gmail.com>
2017-02-04 23:48:46 -0500

    Clean up Firebase middleware code.

--- src/backend/middleware/firebase.js
+++ src/backend/middleware/firebase.js
@@ -9,37 +9,40 @@ import {
 
 const firebaseMiddleware = firebase => store => next => action => {
   const firebaseCommand = action.firebase
-
   const state = store.getState()
 
-  if (firebaseCommand) {
-    if (firebaseCommand !== LOGIN && firebase.auth.currentUser == undefined) {
-      console.error('User is not signed in')
-      return
-    }
+  // Early return if nothing for Firebase
+  if (firebaseCommand == undefined) {
+    return next(action)
+  }
 
-    switch (firebaseCommand) {
-      case SAVE_PREFERENCES:
-        setTimeout(() => firebase.savePreferences(store.getState()), 0)
-        break
-      case LOAD_MESSAGES:
-        firebase.loadMessages()
-        break
-      case LOGIN:
-        firebase.login(action)
-        break
-      case LOGOUT:
-        firebase.logout(state)
-        break
-      case SEND_MESSAGE:
-        firebase.sendMessage(state, action)
-        break
-      case UPDATE_USER:
-        firebase.updateUser(action)
-        break
-      default:
-        break
-    }
+  // Error on Firebase-y actions if there's no logged-in user
+  if (firebaseCommand !== LOGIN && firebase.auth.currentUser == undefined) {
+    console.error('User is not signed in')
+    return
+  }
+
+  switch (firebaseCommand) {
+    case SAVE_PREFERENCES:
+      setTimeout(() => firebase.savePreferences(store.getState()), 0)
+      break
+    case LOAD_MESSAGES:
+      firebase.loadMessages()
+      break
+    case LOGIN:
+      firebase.login(action)
+      break
+    case LOGOUT:
+      firebase.logout(state)
+      break
+    case SEND_MESSAGE:
+      firebase.sendMessage(state, action)
+      break
+    case UPDATE_USER:
+      firebase.updateUser(action)
+      break
+    default:
+      break
   }
 
   next(action)
