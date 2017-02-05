06acbe3a5b547f38fb4f691391755549f77f0a82
Jason Nall <jsonnull@gmail.com>
2017-02-05 08:28:39 -0500

    Clean up webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -10,34 +10,6 @@ const babel = require('@webpack-blocks/babel6')
 const postcss = require('@webpack-blocks/postcss')
 const cssModules = require('@webpack-blocks/css-modules')
 
-// function postcss (options) {
-  // options = options || {}
-  // const exclude = options.exclude || /\/node_modules\//
-
-  // return (context) => ({
-    // module: {
-      // loaders: [
-        // {
-          // test: context.fileType('text/css'),
-          // exclude: Array.isArray(exclude) ? exclude : [exclude],
-          // use: [
-            // { loader: 'style-loader' },
-            // { loader: 'css-loader' },
-            // {
-              // loader: 'postcss-loader',
-              // options: {
-                // parser: options.parser,
-                // stringifier: options.stringifier,
-                // syntax: options.syntax
-              // }
-            // }
-          // ]
-        // }
-      // ]
-    // }
-  // })
-// }
-
 module.exports = createConfig([
   entryPoint('./src/index.js'),
   setOutput('./public/bundle.js'),
