68648b89d4d1e7cd9b90ea33de8a236ca99e7041
Jason Nall <jsonnull@gmail.com>
2017-02-14 13:08:16 -0500

    Add edit hint to editable component.

--- src/app/components/Editable/Editable.js
+++ src/app/components/Editable/Editable.js
@@ -1,21 +1,39 @@
+/* @flow */
 import React from 'react'
+import styles from './style.css'
 
-export default class Editable extends React.Component {
-  constructor (props) {
+type Props = {
+  value: string,
+  className: string,
+  onChange: Function
+}
+
+type State = {
+  value: string,
+  hover: boolean,
+  mode: 'normal' | 'edit'
+}
+
+export default class Editable extends React.Component<*, Props, *> {
+  state: State
+  shouldFocus: boolean
+
+  constructor (props: Props) {
     super(props)
     this.state = {
       value: this.props.value,
+      hover: false,
       mode: 'normal'
     }
     this.shouldFocus = false
   }
 
-  handleEdit (event) {
+  handleEdit (event: Object) {
     this.setState({ value: event.target.value})
   }
 
-  handleSubmit (event) {
-    this.setState({ mode: 'normal' })
+  handleSubmit () {
+    this.setState({ mode: 'normal', hover: false })
     if (typeof this.props.onChange == 'function') {
       this.props.onChange(this.state.value)
     }
@@ -26,10 +44,18 @@ export default class Editable extends React.Component {
     this.shouldFocus = true
   }
 
-  handleKeyPress (event) {
+  handleKeyPress (event: Object) {
     if (event.charCode == 13) {
       this.handleSubmit()
     }
+  }
+
+  handleMouseEnter () {
+    this.setState({ hover: true })
+  }
+
+  handleMouseLeave () {
+    this.setState({ hover: false })
   }
 
   componentDidUpdate () {
@@ -43,11 +69,18 @@ export default class Editable extends React.Component {
 
   render () {
     if (this.state.mode == 'normal') {
+      const background = <div className={styles.background}>
+        <i className='fa fa-pencil'></i>
+      </div>
+
       return <div
-        className={this.props.className}
+        className={this.props.className + ' ' + styles.editable}
         onClick={e => this.handleClick()}
+        onMouseEnter={() => this.handleMouseEnter()}
+        onMouseLeave={() => this.handleMouseLeave()}
       >
         { this.state.value }
+        { this.state.hover? background : null }
       </div>
     } else {
       return <input
