5ac23a591d3448d5699213d55785f325fac62425
Jason Nall <jsonnull@gmail.com>
2017-03-17 09:02:48 -0400

    Add integration with redux-devtools-extension.

--- src/store.js
+++ src/store.js
@@ -30,7 +30,10 @@ export default function createStoreWithMiddleware () {
     user
   })
 
-  let store = createStore(reducers)
+  let store = createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 
   // Log the initial state
   console.log(store.getState())
