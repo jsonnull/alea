79e732d17c962bd2665354a877e27090f721f805
Jason Nall <jsonnull@gmail.com>
2017-03-18 08:01:48 -0400

    Remove webpack-blocks.

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,54 @@
-const {
-  addPlugins,
-  createConfig,
-  customConfig,
-  env,
-  entryPoint,
-  setOutput,
-  sourceMaps
-} = require('@webpack-blocks/webpack2')
-const babel = require('@webpack-blocks/babel6')
-const postcss = require('@webpack-blocks/postcss')
-const cssModules = require('@webpack-blocks/css-modules')
-const extractText = require('@webpack-blocks/extract-text2')
 const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const DirectoryNamedPlugin =  require('directory-named-webpack-plugin')
 const BabiliPlugin = require('babili-webpack-plugin')
 
-const directoryNamed = () => (context) => ({
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: './public',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [ /\/node_modules\// ],
+        use: ['babel-loader?cacheDirectory']
+      },
+      {
+        test: /\.css*/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader?modules&importLoaders=1',
+            'postcss-loader'
+          ]
+        })
+      }
+    ]
+  },
   resolve: {
+    modules: ['node_modules', 'src'],
     plugins: [
       new DirectoryNamedPlugin()
     ]
-  }
-})
-
-const customResolve = () => (context) => ({
-  resolve: {
-    modules: ['node_modules', 'src']
-  }
-})
-
-const babili = () => (context) => ({
+  },
   plugins: [
-    new BabiliPlugin()
-  ]
-})
-
-module.exports = createConfig([
-  entryPoint('./src/index.js'),
-  setOutput('./public/bundle.js'),
-  babel(),
-  postcss(),
-  cssModules(),
-  addPlugins([
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
       }
-    })
-  ]),
-  env('development', [
-    sourceMaps()
-  ]),
-  env('production', [
-    babili()
-  ]),
-  directoryNamed(),
-  customResolve(),
-  extractText('style.css'),
-  customConfig({
-    stats: {
-      children: false
-    }
-  })
-])
+    }),
+    new ExtractTextPlugin('style.css')
+  ],
+  stats: {
+    children: false
+  }
+}
+
+if (process.env.NODE_ENV == 'production') {
+  config.plugins.push(new BabiliPlugin())
+}
+
+module.exports = config
