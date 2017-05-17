269e2ba5fa57c8e545e659f7acadb1ab00e98bf6
Jason Nall <jsonnull@gmail.com>
2017-05-17 07:35:05 -0400

    Finish up roll result formatting.

--- src/app/Chat/Message/MessageResult.js
+++ src/app/Chat/Message/MessageResult.js
@@ -1,52 +1,83 @@
 /* @flow */
 import React from 'react'
-import type { MessageResult } from 'types'
+import type { MessageResult, Roll } from 'types'
 import styles from './style.css'
 
-type RollsProps = {
-  roll: Object<any>
+type RollProps = {
+  result: number,
+  operation: '+' | '-',
+  color?: 'inherit' | 'green' | 'red',
+  showOperation?: boolean
 }
-const Roll = (props: RollsProps) => {
-  const { die, result, mod, operation } = props.roll
-  let resultColor = 'inherit'
-  if (result === die) {
-    resultColor = 'green'
-  } else if (result === 1) {
-    resultColor = 'red'
-  }
-  return <span>
+const Result = (props: RollProps) => {
+  const { result, operation, color = 'inherit', showOperation = true } = props
 
-    {' '}
-    <span className={styles.roll} style={{color: resultColor}}>
+  const shownOperation = showOperation ? ` ${operation} ` : ''
+
+  return <span>
+    { shownOperation }
+    <span className={styles.roll} style={{color}}>
       { result }
     </span>
   </span>
 }
 
+type RollContainerProps = {
+  roll: Roll | number,
+  isFirst: boolean
+}
+const RollContainer = (props: RollContainerProps) => {
+  const { roll, isFirst } = props
+
+  // Handle the general case where the roll is a constant
+  if (typeof roll === 'number') {
+    return <Result result={roll} operation='+' showOperation={!isFirst} />
+  }
+
+  const { die, result, operation } = roll
+
+  let color = 'inherit'
+  if (result === die) {
+    color = 'green'
+  } else if (result === 1) {
+    color = 'red'
+  }
+
+  return <Result result={result} color={color} operation={operation} showOperation={!isFirst} />
+}
+
 type Props = {
   result: ?MessageResult
 }
-const Result = (props: Props) => {
+const Results = (props: Props) => {
   if (!props.result) {
     return null
   }
 
   const results = props.result
-  let total
+  const total = results.reduce((acc, roll) => {
+    if (typeof roll === 'number') {
+      return acc + roll
+    } else {
+      return acc + roll.result
+    }
+  }, 0)
 
   return (
     <div className={styles.result}>
       <i className='fa fa-cube'></i>
+      {' '}
       {results.map((result, i, arr) => (
         <span key={i}>
-          <Roll roll={result} />
-          {(i + 1 !== arr.length) ? ` ${result.operation} ` : null}
+          <RollContainer roll={result} isFirst={i === 0}/>
         </span>
       ))}
       {' = '}
-      <span className={styles.total}></span>
+      <span className={styles.total}>
+        {total}
+      </span>
     </div>
   )
 }
 
-export default Result
+export default Results
