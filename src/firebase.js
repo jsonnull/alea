5ef48da6136468edc56ed7dd6b32cd51ef2b83d3
Jason Nall <jsonnull@gmail.com>
2017-05-20 10:39:33 -0400

    Fix issue where app did not load for logged-out user.

--- src/firebase.js
+++ src/firebase.js
@@ -10,6 +10,7 @@ const initialize = (config: Object, store: Object) => {
       store.dispatch({ type: 'USER_LOGGED_IN' })
     } else {
       store.dispatch({ type: 'LOGOUT' })
+      store.dispatch({ type: 'APP_FINISHED_LOADING' })
     }
   })
 }
