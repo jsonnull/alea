7ec9eeec9d0d332a07eff05712339ce514b94653
Jason Nall <jsonnull@gmail.com>
2017-11-05 19:40:36 -0500

    fix(map): Map component no longer relies on inline styles.

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -4,24 +4,14 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   background-color: ${props => props.theme.map};
-  box-shadow: inset 0 0 15px 0 rgba(0, 0, 0, 0.2);
+  box-shadow: inset 0 0 5px 0 rgba(0, 0, 0, 0.2);
   flex: 1;
 `
 
-type Props = {
-  sidebar: boolean,
-  chat: boolean
-}
+type Props = {}
 class Map extends React.Component<Props> {
   render() {
-    const { sidebar, chat } = this.props
-
-    let style = {
-      right: chat ? '300px' : '0',
-      left: sidebar ? '300px' : '0'
-    }
-
-    return <Container style={style} />
+    return <Container />
   }
 }
 
--- src/containers/Map.js
+++ src/containers/Map.js
@@ -1,17 +1,4 @@
 // @flow
-import React from 'react'
-import styled from 'styled-components'
-import { connect } from 'react-redux'
 import Map from '../components/Map'
-import type { State } from 'store'
 
-type Props = {
-  sidebar: boolean,
-  chat: boolean
-}
-const mapStateToProps = (state: State): Props => ({
-  sidebar: state.sidebar.open,
-  chat: state.user.preferences.chatPinned
-})
-
-export default connect(mapStateToProps)(Map)
+export default Map
