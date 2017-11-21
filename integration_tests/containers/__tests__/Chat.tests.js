9ad8674a192a2177e24344161bce0f016d26fd11
Jason Nall <jsonnull@gmail.com>
2017-11-21 11:06:45 -0500

    test(Chat): Add integration test for Chat.

---
+++ integration_tests/containers/__tests__/Chat.tests.js
@@ -0,0 +1,47 @@
+// @flow
+import React from 'react'
+import { Provider } from 'react-redux'
+import { mount } from 'enzyme'
+import { ConnectedRouter, push } from 'react-router-redux'
+import { sendMessage, receiveMessage } from '../../../src/actions'
+import setupStore, { history, dispatchSpy } from '../../setupStore'
+import Chat from '../../../src/containers/Chat'
+
+describe('Chat container', () => {
+  const store = setupStore()
+  const message = {
+    key: 'unique',
+    from: 'test1',
+    text: 'messageText',
+    timestamp: 0,
+    result: undefined
+  }
+  store.dispatch(receiveMessage(message))
+
+  const wrapper = mount(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Chat />
+      </ConnectedRouter>
+    </Provider>
+  )
+
+  it('should show messages', () => {
+    expect(wrapper.find('MessageView')).toHaveLength(1)
+    expect(wrapper.text()).toContain('messageText')
+  })
+
+  it('should allow user to toggle pin state', () => {
+    expect(store.getState().user.preferences.chatPinned).toBe(false)
+    wrapper.find('i.fa-thumb-tack').simulate('click')
+    expect(store.getState().user.preferences.chatPinned).toBe(true)
+  })
+
+  it('should allow the user to send messages', () => {
+    wrapper
+      .find('textarea')
+      .simulate('change', { target: { value: 'Test message' } })
+      .simulate('keyUp', { key: 'Enter' })
+    expect(dispatchSpy.calledWith(sendMessage('Test message'))).toBe(true)
+  })
+})
--- src/containers/Chat/index.js
+++ src/containers/Chat/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import Chat from '../../components/Chat'
 import { sendMessage } from 'actions'
+import { TOGGLE_CHAT_PIN } from 'actions/types'
 import type { Message, ThemeName } from 'types'
 import type { State } from 'store'
 
@@ -21,7 +22,7 @@ type DispatchProps = {
 }
 const mapDispatchToProps = (dispatch: Function): DispatchProps => ({
   sendMessage: (text: string) => dispatch(sendMessage(text)),
-  toggleChatPin: () => dispatch({ type: 'TOGGLE_CHAT_PIN' })
+  toggleChatPin: () => dispatch({ type: TOGGLE_CHAT_PIN })
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Chat)
