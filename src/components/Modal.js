939e34c1b939db2b4474ec04e57e3b225e961665
Jason Nall <jsonnull@gmail.com>
2017-12-03 17:38:32 -0500

    fix(settings): Fix settings animation sometimes not present.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -15,26 +15,24 @@ const transitionFromState = (state: TransitionState, transitions: Object) => {
 }
 
 const Background = styled.div`
+  display: flex;
   position: fixed;
   top: 0;
+  right: 0;
   bottom: 0;
   left: 0;
-  right: 0;
   background: ${props => props.theme.modalOverlay};
   opacity: ${props =>
     transitionFromState(props.state, {
-      entering: '0',
+      entering: '1',
       entered: '1',
       exiting: '0',
       exited: '0'
     })};
-  transition: all 0.2s;
+  transition: opacity 0.2s;
   transition-timing-function: cubic-bezier(0.25, 0.46, 0.45, 0.94);
-  display: ${props =>
-    transitionFromState(props.state, {
-      exited: 'none',
-      default: 'flex'
-    })};
+  transform: ${props =>
+    props.state === 'exited' ? 'translateX(-100000px)' : 'none'};
 `
 
 const Wrapper = styled.div`
@@ -48,7 +46,7 @@ const Wrapper = styled.div`
   background-color: ${props => props.theme.modalBackground};
   transform: ${props =>
     transitionFromState(props.state, {
-      entering: 'scale(0.7)',
+      entering: 'scale(1)',
       entered: 'scale(1)',
       exiting: 'scale(0.7)',
       exited: 'scale(0.7)'
@@ -83,12 +81,15 @@ const Modal = (props: ModalProps) => {
   const { show, ...restProps } = props
 
   return (
-    <Transition in={show} timeout={{ enter: 17, exit: 200 }}>
-      {(state: TransitionState) => (
-        <Background state={state}>
-          <ContentWithClick state={state} {...restProps} />
-        </Background>
-      )}
+    <Transition in={show} timeout={{ enter: 200, exit: 200 }}>
+      {(state: TransitionState) => {
+        console.log(state)
+        return (
+          <Background state={state}>
+            <ContentWithClick state={state} {...restProps} />
+          </Background>
+        )
+      }}
     </Transition>
   )
 }
