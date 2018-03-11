a624ee8bc84f216370146470f4aeab18a6f697e1
Jason Nall <jsonnull@gmail.com>
2018-03-11 18:22:58 -0400

    Move jest config to separate file.

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  setupFiles: [
+    '<rootDir>/scripts/jest/shim.js',
+    '<rootDir>/scripts/jest/setupTests.js'
+  ],
+  testMatch: ['**/__tests__/**/*.tests.js?(x)'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/scripts/jest/fileMock.js'
+  },
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  }
+}
