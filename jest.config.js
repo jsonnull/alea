54cfafc1097601bcf4dfb7e61c946160b71018bb
Jason Nall <jsonnull@gmail.com>
2018-03-15 20:32:52 -0400

    Fix styled-components snapshot tests.

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@ module.exports = {
     '<rootDir>/scripts/jest/shim.js',
     '<rootDir>/scripts/jest/setupTests.js'
   ],
+  setupTestFrameworkScriptFile: '<rootDir>/scripts/jest/setupFramework.js',
+  snapshotSerializers: ['enzyme-to-json/serializer'],
   testMatch: ['**/__tests__/**/*.tests.js?(x)'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
